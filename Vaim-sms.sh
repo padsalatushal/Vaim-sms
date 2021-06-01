cfe4d796f641ab70e8bc53bdcf999b8c1b1233c1
Padsala Trushal <57517785+Trushal2004@users.noreply.github.com>
2021-06-01 16:42:16 +0530

    Add files via upload

---
+++ Vaim-sms.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#==========================================================================================================================
+#========================================================================================================================== 
+#Vaim-sms v1.0
+#don't use my code with giving me credit 
+#if you use without giving me credit then you won't became a programmer so please respect
+#==========================================================================================================================
+#==========================================================================================================================
+
+
+
+
+clear
+printf "\e[1;91m    ____   ____      .__\e[0m\n"
+printf "\e[1;91m    \   \ /   /____  |__| _____      \e[0m\e[1;93m       ______ _____   ______ \e[0m\n "
+printf "\e[1;91m     \   Y   /\__  \ |  |/     \   __\e[0m\e[1;93m____  /  ___//     \ /  ___/ \e[0m\n "
+printf "\e[1;91m      \     /  / __ \|  |  Y Y  \ /__\e[0m\e[1;93m___/  \___ \|  Y Y  \\___ \  \e[0m\n "
+printf "\e[1;91m       \___/  (____  /__|__|_|  /    \e[0m\e[1;93m     /____  >__|_|  /____  > \e[0m\n "
+printf "\e[1;91m                   \/         \/     \e[0m\e[1;93m          \/      \/     \/  \e[0m\n "
+
+printf "\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[1;31m           By Padsala Trushal  \e[0m                            \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf "\n"
+printf "        \e[1;91m Disclaimer: this tool is designed for Prank\n"
+printf "         testing in an authorized simulated cyberattack\n"
+printf "         Attacking targets without prior mutual consent\n"
+printf "         is illegal!\n"
+printf "\n"
+
+
+
+
+printf "\e[1;92m[\e[0m +\e[1;92m ]\e[0m \e[1;93mENTER VICITM NUMBER >>> \e[0m\n"
+read -p '---------------> ' num
+printf "\e[1;92m[\e[0m +\e[1;92m ]\e[0m \e[1;93mENTER LOG FILE NAME >>> \e[0m\n"
+read -p '---------------> ' log
+
+
+printf ">>>\e[1;92m  SEARCHING +91$num \e[0m<<<"
+printf "\n"
+printf ">>>\e[1;92m IP TRACKING START ON +91$num \e[0m<<<"
+printf "\n"
+printf ">>>\e[1;92m GLOBAL SEARCH DONE FOR +91$num \e[0m<<<"
+printf "\n"
+printf ">>>\e[1;92m IP TRACKING  DONE FOR +91$num \e[0m<<<"
+printf "\n"
+printf ">>>\e[1;92m SHOWING RESULTS FOR +91$num \e[0m<<<"
+printf "\n"
+printf "\n"
+printf "\e[1;92m Location \e[0m : Not Found"
+printf "\n"        
+printf "\e[1;92m IP ADDRESS \e[0m : 2405:205:c96d:1fac:705:125b:3b3:6de"        
+printf "\n"
+printf "\e[1;92m OWNER NAME \e[0m : Kano Dhudhat"
+printf "\e[1;92m SIM OPERATOR \e[0m : Airtel"
+printf "\n"
+printf "\e[1;92m VICTIM TTL  \e[0m : Gujarat, India"
+printf "\n"
+
+
+
+
+
